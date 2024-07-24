792a4c940cb40959ead81d36d167862f3b84637c
radman1007 <radman.cod110@gmail.com>
2024-07-24 23:53:02 +0330

    add put

--- accounts/views.py
+++ accounts/views.py
@@ -10,3 +10,10 @@ class UserView(APIView):
         user = request.user
         serializer = UserSerializer(instance=user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
