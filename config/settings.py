5f5977b52201890a7eaeb30a4e8393f0093e3881
radman1007 <radman.cod110@gmail.com>
2024-07-24 23:09:38 +0330

    debug

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AUTH_USER_MODEL = 'accounts.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
