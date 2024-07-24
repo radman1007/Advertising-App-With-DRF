05e8d1f7461f2516923831e6fa258e31435c6710
radman1007 <radman.cod110@gmail.com>
2024-07-24 23:13:29 +0330

    add user view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .serializer import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
+
+class UserView(APIView):
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(instance=user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
