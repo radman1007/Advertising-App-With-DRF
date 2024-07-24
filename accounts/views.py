64853ed76f91e0f3be4c4b95abdc4b3ee0ffbd0f
radman1007 <radman.cod110@gmail.com>
2024-07-24 23:56:04 +0330

    debug

--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,15 @@ from rest_framework import status
 
 
 class UserView(APIView):
+    serializer_class = UserSerializer
     def get(self, request):
         user = request.user
         serializer = UserSerializer(instance=user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def put(self, request):
-        serializer = UserSerializer(data=request.data)
+        user = request.user
+        serializer = UserSerializer(instance=user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
