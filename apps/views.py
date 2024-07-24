dcc443ed81273ff64baa84910bcbbd7c6cee6f43
radman1007 <radman.cod110@gmail.com>
2024-07-25 00:19:05 +0330

    add view for ads

--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Ad
+from .serializers import AdSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class AdListView(APIView):
     def get(self, request):
         queryset = Ad.objects.filter(is_publish=True)
+        serializer = AdSerializer(instance=queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
