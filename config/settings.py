e0d2f7b8c7f655932d1f2c97c2a77442685c8f68
radman1007 <radman.cod110@gmail.com>
2024-07-24 21:16:14 +0330

    debug

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
